e51980a58e6ea86876ddf23d0ed15e62e26d86d4
Gleb Kalinin <50992188+DedushkaDed@users.noreply.github.com>
2020-06-04 12:35:30 +0500

    QrScannerFragment - Чтение Qr кода, отображение камеры. QrScannerFragment - Добавил flashlight иконку.

--- app/src/main/java/gleb/kalinin/qrcodescanner/ui/scanner/QrScannerFragment.kt
+++ app/src/main/java/gleb/kalinin/qrcodescanner/ui/scanner/QrScannerFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.zxing.Result
 
 import gleb.kalinin.qrcodescanner.R
+import kotlinx.android.synthetic.main.fragment_qr_scanner.view.*
+import me.dm7.barcodescanner.zxing.ZXingScannerView
 
 
-class QrScannerFragment : Fragment() {
+class QrScannerFragment : Fragment(), ZXingScannerView.ResultHandler {
 
     companion object {
         fun newInstance() : QrScannerFragment {
@@ -17,8 +22,74 @@ class QrScannerFragment : Fragment() {
         }
     }
 
+    private lateinit var mView: View
+
+    private lateinit var scannerView: ZXingScannerView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        mView =  inflater.inflate(R.layout.fragment_qr_scanner, container, false)
+        initializeQrScanner()
+        onClicks()
+        return mView.rootView
+    }
+
+    private fun onClicks() {
+        mView.flashToggle.setOnClickListener {
+            if(it.isSelected){
+                onFlashLight()
+            } else {
+                offFlashLight()
+            }
+        }
+    }
+
+    private fun offFlashLight() {
+        mView.flashToggle.isSelected = false
+        scannerView.flash = false
+    }
+
+    private fun onFlashLight() {
+        mView.flashToggle.isSelected = true
+        scannerView.flash = true
+    }
+
+    private fun initializeQrScanner() {
+        scannerView = ZXingScannerView(context!!)
+        scannerView.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorTranslucent))
+        scannerView.setBorderColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setLaserColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setBorderStrokeWidth(10)
+        scannerView.setAutoFocus(true)
+        scannerView.setSquareViewFinder(true)
+        scannerView.setResultHandler(this)
+        // Передаем в UI
+        mView.containerScanner.addView(scannerView)
+        startQrCamera()
+
+    }
+
+    private fun startQrCamera(){
+        scannerView.startCamera()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scannerView.setResultHandler (this)
+        scannerView.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView.stopCamera()
+    }
+
+    override fun handleResult(rawResult: Result?) {
+        Toast.makeText(context!!, rawResult?.text, Toast.LENGTH_SHORT).show()
     }
 
 }
